7c366664c9a4af98bb5284d6abbee03a8b86885e
Sitesh Kumar <thesiteshkumargmail.com>
2023-11-06 15:35:28 +0530

    Toast Notifications

--- 9-Toast-Notification/script.js
+++ 9-Toast-Notification/script.js
@@ -1,27 +1,50 @@
-let toastBox = document.getElementById("toastBox");
-
-let successMsg = `<i class="fa-solid fa-circle-check"></i> Successfully Submitted`;
-let errorMsg = `<i class="fa-solid fa-circle-exclamation"></i> Please fix the errors!`;
-let invalidMsg = `<i class="fa-solid fa-triangle-exclamation"></i> Invalid Input`;
-
-function showToast(msg) {
-  let toast = document.createElement("div");
-  toast.classList.add("toast");
-  toast.innerHTML = msg;
-  toastBox.appendChild(toast);
-  console.log(msg);
-
-  if (msg.includes("invalid")) {
-    toast.classList.add("invalid");
-    console.log(toast.classList)
-  }
-  if (msg.includes("error")) {
-    toast.classList.add("error");
-    console.log(toast.classList)
-  }
-
-
-    setTimeout(() => {
-        toast.remove();
-    }, 3000);
-}
+let notifications = document.querySelector('.notifications');
+    let success = document.getElementById('success');
+    let error = document.getElementById('error');
+    let warning = document.getElementById('warning');
+    let info = document.getElementById('info');
+    
+    function createToast(type, icon, title, text){
+        let newToast = document.createElement('div');
+        newToast.innerHTML = `
+            <div class="toast ${type}">
+                <i class="${icon}"></i>
+                <div class="content">
+                    <div class="title">${title}</div>
+                    <span>${text}</span>
+                </div>
+                <i class="fa-solid fa-xmark" onclick="(this.parentElement).remove()"></i>
+            </div>`;
+        notifications.appendChild(newToast);
+        newToast.timeOut = setTimeout(
+            ()=>newToast.remove(), 5000
+        )
+    }
+    success.onclick = function(){
+        let type = 'success';
+        let icon = 'fa-solid fa-circle-check';
+        let title = 'Success';
+        let text = 'This is a success toast.';
+        createToast(type, icon, title, text);
+    }
+    error.onclick = function(){
+        let type = 'error';
+        let icon = 'fa-solid fa-circle-exclamation';
+        let title = 'Error';
+        let text = 'This is a error toast.';
+        createToast(type, icon, title, text);
+    }
+    warning.onclick = function(){
+        let type = 'warning';
+        let icon = 'fa-solid fa-triangle-exclamation';
+        let title = 'Warning';
+        let text = 'This is a warning toast.';
+        createToast(type, icon, title, text);
+    }
+    info.onclick = function(){
+        let type = 'info';
+        let icon = 'fa-solid fa-circle-info';
+        let title = 'Info';
+        let text = 'This is a info toast.';
+        createToast(type, icon, title, text);
+    }
